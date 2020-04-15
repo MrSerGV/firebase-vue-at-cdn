6b3346eb3641144afb1e9572ae6d6dbab6452608
Sergej Gritsay <sergej.gritsay@gmail.com>
2020-04-15 19:47:54 +0300

    add new features

---
+++ README.md
@@ -0,0 +1,16 @@
+# Music charts
+
+**Technologies used**
+
+- Firebase Functions
+- Firebase Authentication
+- Cloud FireStore
+- Firebase Hosting
+- Vue at CDN
+- HTML & CSS
+
+**Features**
+- Register/Login user
+- Track new users and new musical composition
+- Only authenticated admins can add new musical composition
+- Only authenticated users can vote up tracks and only up once
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,92 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+  // for background triggers you must return a value/promise
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.set({
+    email: user.email,
+    upvotedOn: [],
+  });
+});
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
+}); 
+
+// http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth.admin) {
+    throw new functions.https.HttpsError(
+      'unauthenticated', 
+      'only authenticated admins can add new musical composition'
+    );
+  }
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+      'invalid-argument', 
+      'request must be no more than 30 characters long'
+    );
+  }
+  return admin.firestore().collection('requests').add({
+    text: data.text,
+    upvotes: 0
+  });
+});
+
+// // upvote callable function
+exports.upvote = functions.https.onCall(async (data, context) => {
+  // check auth state
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated', 
+      'only authenticated users can vote up musical composition'
+    );
+  }
+  // get refs for user doc & request doc
+  const user = admin.firestore().collection('users').doc(context.auth.uid);
+  const request = admin.firestore().collection('requests').doc(data.id);
+
+  const doc = await user.get();
+  // check thew user hasn't already upvoted
+  if(doc.data().upvotedOn.includes(data.id)){
+    throw new functions.https.HttpsError(
+      'failed-precondition', 
+      'You can only vote something up once'
+    );
+  }
+
+  // update the array in user document
+  await user.update({
+    upvotedOn: [...doc.data().upvotedOn, data.id]
+  });
+
+  // update the votes on the request
+  return request.update({
+    upvotes: admin.firestore.FieldValue.increment(1)
+  });
+
+});
+
+// firestore trigger for tracking activity
+exports.logActivities = functions.firestore.document('/{collection}/{id}')
+  .onCreate((snap, context) => {
+    console.log(snap.data());
+
+    const activities = admin.firestore().collection('activities');
+    const collection = context.params.collection;
+
+    if (collection === 'requests') {
+      return activities.add({ text: 'a new musical composition was added' });
+    }
+    if (collection === 'users') {
+      return activities.add({ text: 'a new user signed up'});
+    }
+
+    return null;
+});
